b85b536e69f9a4121445e1587155b39d9bcf90a6
Piero Coronado Flores <pacoronado01@gmail.com>
2026-03-03 19:04:13 -0500

    Add commandlinerunner for db connection test

--- src/main/java/com/oreip/LanguageStudyTracker/LanguageStudyTrackerApplication.java
+++ src/main/java/com/oreip/LanguageStudyTracker/LanguageStudyTrackerApplication.java
@@ -1,7 +1,10 @@
 package com.oreip.LanguageStudyTracker;
 
+import com.oreip.LanguageStudyTracker.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LanguageStudyTrackerApplication {
@@ -10,4 +13,11 @@ public class LanguageStudyTrackerApplication {
 		SpringApplication.run(LanguageStudyTrackerApplication.class, args);
 	}
 
+//	@Bean
+//	public CommandLineRunner commandLineRunner(UserRepository userRepository) {
+//		return runner -> {
+//			System.out.println("Testing DB connection...");
+//			System.out.println(userRepository.findAll());
+//		};
+//	}
 }
