e043e566884ed60ae0efbd7b21c81bb8c41c53b9
Piero Coronado Flores <pacoronado01@gmail.com>
2026-03-04 18:56:59 -0500

    Rename methods for consistency

--- src/main/java/com/oreip/LanguageStudyTracker/service/StudySessionService.java
+++ src/main/java/com/oreip/LanguageStudyTracker/service/StudySessionService.java
@@ -5,9 +5,9 @@ import com.oreip.LanguageStudyTracker.entity.StudySession;
 import java.util.List;
 
 public interface StudySessionService {
-    List<StudySession> findSessionsByUserId(int theId);
+    List<StudySession> findByUserId(int theId);
 
-    List<StudySession> findSessionsByUserIdAndLanguageId(int userId, int languageId);
+    List<StudySession> findByUserIdAndLanguageId(int userId, int languageId);
 
-    StudySession save(StudySessionService theSession);
+    StudySession save(StudySession theSession);
 }
