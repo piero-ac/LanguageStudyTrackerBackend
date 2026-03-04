a2573f77a4a934867f69ea7245f6a475bac850a2
Piero Coronado Flores <pacoronado01@gmail.com>
2026-03-04 18:56:49 -0500

    Add methods for custom queries using method naming convention

--- src/main/java/com/oreip/LanguageStudyTracker/repository/StudySessionRepository.java
+++ src/main/java/com/oreip/LanguageStudyTracker/repository/StudySessionRepository.java
@@ -3,5 +3,11 @@ package com.oreip.LanguageStudyTracker.repository;
 import com.oreip.LanguageStudyTracker.entity.StudySession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudySessionRepository extends JpaRepository<StudySession, Integer> {
+    List<StudySession> findByUserId(int theId);
+
+    List<StudySession> findByUserIdAndLanguageId(int userId, int languageId);
+
 }
