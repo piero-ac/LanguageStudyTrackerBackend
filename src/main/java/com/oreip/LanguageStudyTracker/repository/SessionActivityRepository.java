67e8370dc9995d8e35d8c37c8d6d36ac8175408b
Piero Coronado Flores <pacoronado01@gmail.com>
2026-03-04 19:08:53 -0500

    Use method naming for finding activities for session id

--- src/main/java/com/oreip/LanguageStudyTracker/repository/SessionActivityRepository.java
+++ src/main/java/com/oreip/LanguageStudyTracker/repository/SessionActivityRepository.java
@@ -4,5 +4,8 @@ import com.oreip.LanguageStudyTracker.entity.SessionActivity;
 import com.oreip.LanguageStudyTracker.entity.SessionActivityId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SessionActivityRepository extends JpaRepository<SessionActivity, SessionActivityId> {
+    List<SessionActivity> findByStudySessionId(int theId);
 }
