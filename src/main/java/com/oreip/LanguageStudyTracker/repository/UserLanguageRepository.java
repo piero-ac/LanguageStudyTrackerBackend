3120e16859e38263f126a7a568d5a4ae8b049d8b
Piero Coronado Flores <pacoronado01@gmail.com>
2026-03-04 19:15:30 -0500

    Add method for finding languages used by user

--- src/main/java/com/oreip/LanguageStudyTracker/repository/UserLanguageRepository.java
+++ src/main/java/com/oreip/LanguageStudyTracker/repository/UserLanguageRepository.java
@@ -4,5 +4,8 @@ import com.oreip.LanguageStudyTracker.entity.UserLanguage;
 import com.oreip.LanguageStudyTracker.entity.UserLanguageId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserLanguageRepository  extends JpaRepository<UserLanguage, UserLanguageId> {
+    List<UserLanguage> findAllByUserId(int theId);
 }
